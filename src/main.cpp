77d7aa1de5dde6b7b4ea259d5784833a5beda5fd
raul-gliga-24 <raulgliga24@gmail.com>
2026-05-16 18:10:29 +0300

    Added styling function

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,36 @@
 #include "core/Label.hpp"
 #include "renderer/Cell.hpp"
 #include "renderer/Buffer.hpp"
+#include "renderer/Renderer.hpp"
 
 int main(){
-    Buffer buf(10,5);
-    buf.at(3,2).ch = 'X';
-     std::cout << buf.at(3, 2).ch << "\n";  // should print X
-    std::cout << buf.at(0, 0).ch << "\n"; 
-}
+
+  /*
+  Key codes:
+
+  ┌──────────────────────┬────────────────┐
+  │        Effect        │    Sequence    │
+  ├──────────────────────┼────────────────┤
+  │ Reset all attributes │ \033[0m        │
+  ├──────────────────────┼────────────────┤
+  │ Bold on              │ \033[1m        │
+  ├──────────────────────┼────────────────┤
+  │ Underline on         │ \033[4m        │
+  ├──────────────────────┼────────────────┤
+  │ Foreground 256-color │ \033[38;5;<n>m │
+  ├──────────────────────┼────────────────┤
+  │ Background 256-color │ \033[48;5;<n>m │
+  └──────────────────────┴────────────────┘
+  */
+    Renderer r(80, 24);
+    r.backBuffer().at(5, 3).ch = 'H';
+    r.backBuffer().at(5, 3).fg = 11;
+    r.backBuffer().at(6, 3).ch = 'i';
+
+    std::cin.get();
+
+
+    r.present();
+    std::cin.get();
+
+ }
